8953016134168c925d0de3dfe2bc50048ec136f7
SalimmReza <salimnuaa@gmail.com>
2022-10-24 22:36:07 +0600

    single course information added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 5000
 // app.use(cors());
 
 const course = require('./data/course.json');
-// const news = require('./data/news.json');
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -16,7 +16,11 @@ app.get('/course', (req, res) => {
     res.send(course)
 })
 
-
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
