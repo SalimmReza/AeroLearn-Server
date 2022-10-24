91ceb8ff2cb1b9764ee7811a6cba6dbe7445a2d7
SalimmReza <salimnuaa@gmail.com>
2022-10-24 22:30:18 +0600

    course data added

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
+// const cors = require('cors');
 const port = 5000
+
+// app.use(cors());
+
+const course = require('./data/course.json');
+// const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.get('/course', (req, res) => {
+    res.send(course)
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
