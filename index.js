9ffd7dd650ea29dc25aca67834fe2e2a2eec635b
SalimmReza <salimnuaa@gmail.com>
2022-10-26 20:19:04 +0600

    git ignore

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-// const cors = require('cors');
+const cors = require('cors');
 const port = 5000
 
-// app.use(cors());
+app.use(cors());
 
 const course = require('./data/course.json');
 
